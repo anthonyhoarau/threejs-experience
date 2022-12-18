6368b4e2122ebaaa4ec2f418d86459478b80797b
anthonyhoarau <1210240+anthonyhoarau@users.noreply.github.com>
2022-12-18 17:16:43 +0100

    initial setup

---
+++ src/Experience.jsx
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+import React from 'react';
+
+export default class Experience extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+
+  componentDidMount() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas.current });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    this.camera.position.z = 5;
+
+    this.animate();
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  render() {
+    return (
+      <canvas ref={this.canvas} />
+    );
+  }
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './normalize.css';
+import './index.css';
+import Experience from './Experience';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Experience />
+  </React.StrictMode>,
+);
